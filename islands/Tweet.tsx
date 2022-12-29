8a63b8ce9279b8adc1f0d591b30c270628c11a6d
newt239 <ysti2019.win2020.st@gmail.com>
2022-12-30 00:16:47 +0900

    fix: tweet url validation

--- islands/Tweet.tsx
+++ islands/Tweet.tsx
@@ -9,6 +9,7 @@ export default function Twitter() {
   const [tweetUrl, setTweetUrl] = useState<string>("");
   const [tweet, setTweet] = useState<Tweet | null>(null);
   const [googlePhotos, setGooglePhotos] = useState<string[]>([]);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     const getAlbumList = async () => {
@@ -22,11 +23,18 @@ export default function Twitter() {
   }, []);
 
   const getTweet = async () => {
-    const id = tweetUrl.split("status/")[1];
-    const url = `./api/twitter?id=${id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    setTweet(data);
+    if (
+      tweetUrl.startsWith("https://twitter.com") ||
+      tweetUrl.startsWith("https://mobile.twitter.com")
+    ) {
+      const id = tweetUrl.split("status/")[1].split("?")[0];
+      const url = `./api/twitter?id=${id}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      setTweet(data);
+    } else {
+      setError(true);
+    }
   };
 
   const saveToGooglePhoto = async () => {
@@ -42,6 +50,17 @@ export default function Twitter() {
       const data = await res.json();
       setGooglePhotos(data);
     }
+  };
+
+  const reset = () => {
+    setTweetUrl("");
+    setTweet(null);
+    setGooglePhotos([]);
+  };
+
+  const closeError = () => {
+    reset();
+    setError(false);
   };
 
   return (
@@ -61,6 +80,31 @@ export default function Twitter() {
           Get!
         </button>
       </div>
+      {error && (
+        <div className="alert alert-error shadow-lg">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>ツイートの取得中にエラーが発生しました。</span>
+          </div>
+          <div className="flex-none">
+            <button onClick={closeError} className="btn btn-sm btn-ghost">
+              閉じる
+            </button>
+          </div>
+        </div>
+      )}
       {tweet && (
         <div>
           <ul class="list-disc m-5">
@@ -114,7 +158,15 @@ export default function Twitter() {
           </ul>
         </div>
       )}
-      <a href="/" class="btn btn-link">{"<"} back home</a>
+      <div class="flex gap-5 items-center">
+        <a href="/" class="btn btn-link">{"<"} back home</a>
+        <button
+          onClick={reset}
+          class="btn btn-error"
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
